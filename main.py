b068cb683363c753a1c0e3cb5460a925d924a1be
Jinda Du <jindadu00@sjtu.edu.cn>
2025-02-25 23:34:38 +0800

    9 sensors

--- main.py
+++ main.py
@@ -11,44 +11,50 @@ print("Starting the encoder reading script...")
 # Prepare plot
 plt.ion()
 fig, ax = plt.subplots()
-x_data, y1_data, y2_data, y3_data = [], [], [], []
+y_data =[[],[],[],[],[],[],[],[],[]]
+
+
+x_data = []
+line=[]
 line1, = ax.plot([], [], label='Encoder 1')
 line2, = ax.plot([], [], label='Encoder 2')
 line3, = ax.plot([], [], label='Encoder 3')
+line4, = ax.plot([], [], label='Encoder 4')
+line5, = ax.plot([], [], label='Encoder 5')
+line6, = ax.plot([], [], label='Encoder 6')
+line7, = ax.plot([], [], label='Encoder 7')
+line8, = ax.plot([], [], label='Encoder 8')
+line9, = ax.plot([], [], label='Encoder 9')
+line.append(line1)
+line.append(line2)
+line.append(line3)
+line.append(line4)
+line.append(line5)
+line.append(line6)
+line.append(line7)
+line.append(line8)
+line.append(line9)
+
+
 ax.set_ylim(0, 360)
 ax.set_xlim(0, 50)
 plt.legend()
 
-# encoder.reset_encoder(1,2048)
-# time.sleep(0.5)
-# encoder.reset_encoder(2,2048)
-# time.sleep(0.5)
-# encoder.reset_encoder(3,2048)
-# time.sleep(0.5)
+ID=[1,2,3,4,5,6,7,8,9]
+# for i in ID:
+#     encoder.reset_encoder(i,2048)
+#     time.sleep(0.1)
+
 
 # Read angles in a loop and update plot
 for i in range(5000):
-    angle1 = encoder.read_angle(1)
-    time.sleep(0.01)
-
-    angle2 = encoder.read_angle(2)
-    time.sleep(0.01)
-
-    angle3 = encoder.read_angle(3)
-    time.sleep(0.01)
-    
     x_data.append(i)
-    y1_data.append(angle1 if angle1 is not None else 0)
-    y2_data.append(angle2 if angle2 is not None else 0)
-    y3_data.append(angle3 if angle3 is not None else 0)
-    
-    # Update plot data
-    line1.set_xdata(x_data)
-    line1.set_ydata(y1_data)
-    line2.set_xdata(x_data)
-    line2.set_ydata(y2_data)
-    line3.set_xdata(x_data)
-    line3.set_ydata(y3_data)
+    for k in ID:
+        angle = encoder.read_angle(k)
+        time.sleep(0.01)
+        y_data[k-1].append(angle if angle is not None else 0)
+        line[k-1].set_xdata(x_data)
+        line[k-1].set_ydata(y_data[k-1])
     
     ax.set_xlim(max(0, i - 50), i + 1)
     fig.canvas.draw()
